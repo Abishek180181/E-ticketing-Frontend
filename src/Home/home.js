7598f3ca6fece29279cc3aafbbe654b78677acf5
sagar <sagarcrcoc@gmail.com>
2021-07-28 21:20:43 +0545

    Design improved

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col,Table } from 'react-bootstrap';
+import home_image from '../assets/logo/home_image.png'
+
 
 const Home = (props) => {
 	let {} = props;
 	return (
 		<React.Fragment>
-			
+			  <h6> This is home page </h6>
 		</React.Fragment>
 	);
 };
--- src/Hospital/editDetails.js
+++ src/Hospital/editDetails.js
@@ -22,6 +22,7 @@ const EditDetails = (props) => {
         "personName":data.personName,
         "latitude":data.locationPoint.coordinates[1],
         "longitude":data.locationPoint.coordinates[0],
+        "description":data.description,
         "config":{
             "headers":{
                 "authorization":`Bearer ${sessionStorage.getItem('token')}`
@@ -159,6 +160,16 @@ const EditDetails = (props) => {
                                                     {hospitalDetails['errors']['longitude']&& (<p> <small style={{color:"red"}}> *{hospitalDetails['errors']['longitude']} </small> </p>)}
                                                 </div>
                                         </Col>
+                                    </Row>
+                                    <Row className='p-2'>
+                                        <Col lg={12} md={12} xs={12}>                                                                           
+                                                <div className="form-group">
+                                                    <label> Description</label>
+                                                    <textarea className="form-control" name="description" onChange={(event)=>{changeHandler(event)}} placeholder="Enter description">{hospitalDetails.description}</textarea>
+                                                    {hospitalDetails['errors']['description']&& (<p> <small style={{color:"red"}}> *{hospitalDetails['errors']['description']} </small> </p>)}
+                                                </div>
+                                        </Col>
+                                        
                                     </Row>
                                     {hospitalDetails['errors']['random']&& (<p> <small style={{color:"red"}}> *{hospitalDetails['errors']['random']} </small> </p>)}
 
