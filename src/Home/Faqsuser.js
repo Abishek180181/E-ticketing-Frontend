696fc56308696fbf2fcf779833cb7524184dbc25
ankit016 <alyohinadhikari@gmail.com>
2021-08-26 16:22:36 +0545

    faqs

--- src/Home/Faqsuser.js
+++ src/Home/Faqsuser.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Col, Container, Row,Jumbotron} from 'react-bootstrap'
 
 const Faqsuser = () => {
   return (
-    <div>
-      this is faq user page
-    </div>
+    <>
+      <Container fluid>
+        <Row>
+          <Col sm={12} className="text-center text-black py-5" style={{backgroundColor:'#f0f0f0'}}>
+            <Jumbotron fluid md={12}>
+              <h5>Frequently asked questions</h5>
+            </Jumbotron>
+
+          </Col>
+          <Col className="mx-auto">
+            <p>qns goes here</p>
+          </Col>
+        </Row>
+      </Container>
+    </>
   )
 }
 
